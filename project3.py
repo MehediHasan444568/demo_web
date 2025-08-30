158d00d657a71e3a90c6557e04c05d1f0316331b
MehediHasan444568 <mehedi.hasan.444568@gmail.com>
2025-08-30 23:26:18 +0600

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas
+
+name= st.text_input("Input your name: ")
+father= st.text_input("Input your father name: ")
+mother = st.text_input("Input your mother name: ")
+text= st.text_area("Enter your text. ")
+age= st.selectbox("Enter your class: ",(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
+button= st.button("Done")
+
+if button:
+    st.write(f"""
+          NAME: {name}\n
+          FATHER NAME: {father}\n
+          MOTHER NAME: {mother}\n
+          TEXT: {text}\n
+          AGE: {age}
+          """)
